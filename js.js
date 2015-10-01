5ab866c387ee0869607d8e9af7abfd04e58860b1
Chekuda <sochekilla@gmail.com>
2015-10-01 17:01:21 +0800

    new files added

--- js.js
+++ js.js
@@ -37,8 +37,30 @@
 window.onload= function ()
     {
 
+    	/******Active menu*******/
+    	//
+    	//
+    	//
+    	/*$(".nav.navbar-nav.topNavOptions li").on("click",activeMenu);
+
+    	function activeMenu(){
+    		if(!$(this).hasClass('active'))
+    		{
+    			$(".nav.navbar-nav.topNavOptions li").removeClass("active");
+    			$(this).addClass("active");
+    		}
+    	}*/
+
+
+    	/*****display bigger images***/
+    	//
+    	//
+    	//
     	$('.chocolat-parent').Chocolat();
-/******Open Carousel*****/  
+		/******Open Carousel*****/  
+		//
+		//
+		//
 		var listButtons = $(".subCategoryGroup .btn");
 
 		for (var lista = 0; lista<listButtons.length; lista++)
@@ -49,25 +71,52 @@ window.onload= function ()
 
 		function showSlider(){
 			//visible the right carousel
+			//
 			if($(this).text() == "SPORT")
 			{
 				$('.carouselChat').css({"visibility":"hidden","height":"0px"});
-				$('.carouselChat.sport').css({"visibility":"visible","height":"auto"})
+				$('.carouselChat.sport').css({"visibility":"visible","height":"auto"});
+
+				$('.carouselveContact').css({"visibility":"hidden","height":"0px"});
+				$('.carouselveContact.sport').css({"visibility":"visible","height":"auto"});
+
+				$('.carouselveContact .flickity-viewport').css({"height":"320px"});
+				$('.carouselveContact img').css({"height":"300px"});
+
+
 			}
 			else if($(this).text() == "TRAVEL")
 			{
 				$('.carouselChat').css({"visibility":"hidden","height":"0px"});
-				$('.carouselChat.travel').css({"visibility":"visible","height":"auto"})
+				$('.carouselChat.travel').css({"visibility":"visible","height":"auto"});
+
+				$('.carouselveContact').css({"visibility":"hidden","height":"0px"});
+				$('.carouselveContact.travel').css({"visibility":"visible","height":"auto"});
+				
+				$('.carouselveContact .flickity-viewport').css({"height":"320px"});
+				$('.carouselveContact img').css({"height":"300px"});
+
 			}
 			else{
 				$('.carouselChat').css({"visibility":"hidden","height":"0px"});
+				$('.carouselveContact').css({"visibility":"hidden","height":"0px"});
+
+				$('.carouselveContact .flickity-viewport').css({"height":"170px"});
+				$('.carouselveContact img').css({"height":"150px"});
+
+
 				alert("there isn't any chat for that at the moment");
 			}
+
+
 
 			console.log("done");
 		}
 
 	/*********Category secction******/
+	//
+	//
+	//
 	$(".subCategoryGroup .btn").on('click',checkActive);
 
 		function checkActive(){
@@ -79,11 +128,14 @@ window.onload= function ()
 		}
 
 
-	/*carousel*****/
+
+
+	/****carousel*****/
+	//
 	var elem = document.querySelector('.main-gallery');
 	var flkty = new Flickity( elem, {
 	  // options
-
+	  //
 	  cellAlign: 'left',
 	  contain: true,
 	  imagesLoaded: true,
@@ -92,7 +144,9 @@ window.onload= function ()
 	});
 
 	/****Go to next page******/
-
+	//
+	//
+	//
 	$(".bottomButtons .menuBottom.btn").on("click", checkTerms);
 
 	function checkTerms(){
